af3b181595eb56071e7eaac07b261627564ce074
PUSHPITHA4738 <lrppushpitha@gmail.com>
2024-09-13 14:43:09 +0530

    finish project

---
+++ app/src/main/java/com/example/waterremainderapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.waterremainderapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.whenCreated
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        }
+    }
